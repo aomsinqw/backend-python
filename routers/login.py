62637a5156022d3c9000c106446e65857692a0ef
Natcha Intapin <natcha.aomsin@gmail.com>
2025-06-20 16:39:00 +0700

    basic login

--- routers/login.py
+++ routers/login.py
@@ -1,14 +1,55 @@
 import hashlib
-
+from jose import JWTError, jwt
+from pydantic import BaseModel
+import json
+from passlib.context import CryptContext
+import os
 from fastapi import APIRouter
+
+SECRET_KEY = "a_very_secret_key_here"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 router = APIRouter()
 
+class User(BaseModel):
+    username: str
+    password: str
+
 @router.get("/{password}")
 def get_users(password : str):
-    hash_object = hashlib.md5(password.encode())  # ต้อง encode เป็น bytes ก่อน
-    md5_hash = hash_object.hexdigest()
+    hash = get_password_hash(password)
+    print("MD5 Hash:", hash)
+    return {"message": hash}
 
-    print("MD5 Hash:", md5_hash)
-    return {"message": md5_hash}
+@router.post("/")
+def login(user: User): 
+    
+    print("Current working directory:", os.getcwd())
+    with open("users.json", "r", encoding="utf-8") as file:
+        data = json.load(file)
 
+    for row in data:
+        # print(row["username"], row["password"])
+        
+        if user.username==row["username"]:
+            result = verify_password(user.password,row['password'])
+            if result :
+                print("Match")
+                return {
+                    "success" : True
+                }
+
+
+    return {
+        "success" : False
+    }
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+# ฟังก์ชันทดสอบ hashing password
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
